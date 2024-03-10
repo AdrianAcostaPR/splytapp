59d2aaddea11b15995c169b417a662a9fb0563a5
AdrianAcostaPR <adrianacostaaponte@gmail.com>
2024-03-09 20:14:16 -0400

    Added tailwindCSS code.

--- src/main/java/splytapp/splytapp/MainController.java
+++ src/main/java/splytapp/splytapp/MainController.java
@@ -11,7 +11,7 @@ public class MainController {
     Stage stage = new Stage();
 
     @FXML
-    private Label chooseFileText; //To resolve alert on main-view.fxml.
+    private Label chooseFileText; //To resolve alert on
 
     @FXML
     private Label welcomeText;
